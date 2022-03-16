4af8057b9fc13392bfa8b69b0e7805750223d965
Francesco Done <45391798+francescodone@users.noreply.github.com>
2022-03-16 21:22:24 +0100

    Added MinimumSwaps

---
+++ interview-preparation-kit/arrays/MinimumSwaps.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumSwaps function below.
+function minimumSwaps(arr) {
+    let swaps = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== (i+1)) {
+            swap(arr, i, arr.findIndex(el => el === (i+1)))
+            swaps++
+        }
+    }
+    return swaps
+}
+
+function swap(arr, i, j) {
+    const tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = minimumSwaps(arr);
+
+    ws.write(res + '\n');
+
+    ws.end();
+}
